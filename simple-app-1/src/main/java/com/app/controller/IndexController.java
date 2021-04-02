c4b228f4eaa55b4a364f07bffab359c8f8071fa5
TanayaKarmakar <karmakar.tanaya@gmail.com>
2021-04-02 12:31:31 +0530

    Application Insights Integration

--- simple-app-1/src/main/java/com/app/controller/IndexController.java
+++ simple-app-1/src/main/java/com/app/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.app.controller;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class IndexController {
+    @Autowired
+    private TelemetryClient telemetryClient;
+
     @GetMapping("/index")
     public String index() {
+        telemetryClient.trackEvent("URI /inddx is triggered - v1");
         return "This is application version V1";
     }
 }
--- simple-app-1/src/main/java/com/app/repositories/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.repositories;
-
-import com.app.domain.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-}
